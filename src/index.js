fd3887374525ef12ea7010c72ce475fb556707bb
Vidsneezes <moraleszez@gmail.com>
2017-04-11 11:47:10 -0600

    remove test

--- src/index.js
+++ src/index.js
@@ -10,23 +10,6 @@ import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters} from './ac
 
 let store = createStore(todoApp);
 
-console.log(store.getState());
-
-let unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
-})
-
-store.dispatch(addTodo("Learn about actions"));
-store.dispatch(addTodo("Learn about reducers"));
-store.dispatch(addTodo("Learn about store"));
-
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETE))
-
-unsubscribe();
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
