6410962119593b9831c8475452f4465075f055ae
Vidsneezes <moraleszez@gmail.com>
2017-04-11 11:14:44 -0600

    modified todoApp in reducer, to  separate state management

--- src/reducers.js
+++ src/reducers.js
@@ -41,18 +41,9 @@ function todos(state = [], action){
     }
 }
 
-function todoApp(state = initialState, action){
-    switch(action.type) {
-        case SET_VISIBILITY_FILTER:
-            return Object.assign({},state,{
-                visibilityFilter: action.filter
-            })
-        case ADD_TODO:
-        case TOGGLE_TODO:
-            return Object.assign({}, state, {
-               todos: todos(state.todos, action)
-            })
-        default:
-            return state
+function todoApp(state = {}, action){
+    return {
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+        todos: todos(state.todos, action)
     }
 }
