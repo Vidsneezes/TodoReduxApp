826e29142487a7412478c108c692fa48549aee40
Vidsneezes <moraleszez@gmail.com>
2017-04-11 12:38:33 -0600

    added TODOs

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './components/App';
 import todoApp from './reducers';
 import './index.css';
 
+//TODO #1 finish by comparing with http://redux.js.org/docs/basics/ExampleTodoList.html
+
+//TODO #2 implemente immutable js
 
 let store = createStore(todoApp);
 
