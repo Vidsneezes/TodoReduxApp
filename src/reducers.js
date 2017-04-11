ec323e3db8843d7f48e1075832694564ceb101f7
Vidsneezes <moraleszez@gmail.com>
2017-04-11 10:56:03 -0600

    added case for set visiibility filter in reducers js

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,5 @@
 import { VisibilityFilters } from './actions'
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from './actions'
 
 const initialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
@@ -6,5 +7,12 @@ const initialState = {
 }
 
 function todoApp(state = initialState, action){
-
+    switch(action.type) {
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({},state,{
+                visibilityFilter: action.filter
+            })
+        default:
+            return state
+    }
 }
