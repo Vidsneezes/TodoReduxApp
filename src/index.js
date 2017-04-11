d08022936fcd33010dff3e54b2a261c53e78dbd9
Vidsneezes <moraleszez@gmail.com>
2017-04-11 11:27:33 -0600

    Added sources

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
+import todoApp from './reducers';
 import './index.css';
+
+let store = createStore(todoApp);
+
 
 ReactDOM.render(
   <App />,
