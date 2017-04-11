de56da15eab772d6b0ddfcbdcf3cab57431bce5e
Vidsneezes <moraleszez@gmail.com>
2017-04-11 11:02:08 -0600

    added case for TOGGLE_TODO

--- src/reducers.js
+++ src/reducers.js
@@ -22,6 +22,17 @@ function todoApp(state = initialState, action){
                     }
                 ]
             })
+        case TOGGLE_TODO:
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo, index) => {
+                    if(index === action.index) {
+                        return Object.assign({},todo,{
+                            completed: !todo.completed
+                        })
+                    }
+                    return todos
+                })
+            })
         default:
             return state
     }
